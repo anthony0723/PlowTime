731eb5fa8849f8dc0fc58622c0ebde0aa7d286c9
Anthony Baldwin <srv0723@bex.net>
2019-02-19 14:06:13 -0500

    First Commit

---
+++ Plow Time/Model/ButtonVC.swift
@@ -0,0 +1,18 @@
+//
+//  ButtonVC.swift
+//  Plow Time
+//
+//  Created by Anthony Baldwin on 2/14/19.
+//  Copyright © 2019 AnthonyBaldwin. All rights reserved.
+//
+
+import UIKit
+
+class ButtonVC: UIButton {
+
+    override func awakeFromNib() {
+        
+        layer.cornerRadius = 5
+
+}
+}
